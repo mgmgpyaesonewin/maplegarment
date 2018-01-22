8a700f8b5b2e81710dba543a420a7a63b854c03c
Pyae Sone <mgmgpyaesonewin@gmail.com>
2018-01-22 22:58:23 +0630

    react init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Alert bsStyle="warning">
+          <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+          good.
+        </Alert>
       </div>
     );
   }
